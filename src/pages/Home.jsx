10f02828e184beab79a42f4967f5d430641c0419
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-10-25 17:55:47 +0200

    feat: Basic global CSS and module setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>WorldWise</h1>
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
