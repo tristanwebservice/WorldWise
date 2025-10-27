04be2c887dec3766bf2e74cf19eb62b5683dc5d0
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-10-27 10:35:56 +0100

    feat: Implement dynamic routes with URL parameters

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = `http://localhost:9000`;
 
@@ -49,11 +50,11 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-
           <Route path="form" element={<p>Form</p>} />
         </Route>
 
