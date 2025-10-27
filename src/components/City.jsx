79c03fd8e651ab8d53ce24dd031da843027b5db4
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-10-27 11:07:45 +0100

    feat: Implement query string for longitude and latitude

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -11,6 +12,10 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get(`lat`);
+  const lng = searchParams.get(`lng`);
+
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
@@ -21,7 +26,15 @@ function City() {
 
   const { cityName, emoji, date, notes } = currentCity;
 
-  return <h1>City {id}</h1>;
+  return (
+    <h1>
+      City {id}
+      <p>
+        Position: {lng}, {lat}
+      </p>
+    </h1>
+  );
+
   // return (
   //   <div className={styles.city}>
   //     <div className={styles.row}>
