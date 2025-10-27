f5f7f9fff4e8584a42e8716f2a0643659d4f6d78
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-10-27 09:05:37 +0100

    feat: Implement nested routes and index route

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,13 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>Welcome!</p>} />
+          <Route path="cities" element={<p>Cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
